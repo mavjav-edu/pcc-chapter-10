9883e0ff52a111e0baa189f961b99c4aa7bab905
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2022-11-13 17:40:11 -0800

    format remember_me

--- remember_me.py
+++ remember_me.py
@@ -1,5 +1,6 @@
 import json
 
+
 def get_stored_username():
     """Get stored username if available."""
     filename = 'username.json'
@@ -11,6 +12,7 @@ def get_stored_username():
     else:
         return username
 
+
 def get_new_username():
     """Prompt for a new username."""
     username = input("What is your name? ")
@@ -18,6 +20,7 @@ def get_new_username():
     with open(filename, 'w') as f:
         json.dump(username, f)
     return username
+
 
 def greet_user():
     """Greet the user by name."""
@@ -27,5 +30,6 @@ def greet_user():
     else:
         username = get_new_username()
         print(f"We'll remember you when you come back, {username}!")
+
 
 greet_user()
